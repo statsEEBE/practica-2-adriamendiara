a09d3eeb320ec26adba58af16fcc4428e43d4bfb
adria.mendiara <adria.mendiara@auleseebe.upc.edu>
2024-09-26 11:28:27 +0200

    problema 1

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,14 @@
 #Codigo para problema 1
+0.1923 # coef de determinació
 
+sqrt(0.1923) #coef de correlació
+
+#pendiente, como aumenta el valor al subir un punto la x
+0.06576
+
+#y= mx+b
+y = 0.06576*647.1+11.79755
+y
+#y = 54.35085
+
+#dado que el coef de determinación es tan cercano a 0 vemos que la dispersión sera muy alta por tanto creo que se trata de la muestra b
